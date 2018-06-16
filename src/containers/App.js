e4e93275faf76722f4e2abd9989abd8facdff512
Ashwin <ashwinr63@gmail.com>
2018-06-16 16:25:45 +0530

    Redux added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList';
 //import {robots} from './robots';
+import { connect } from 'react-redux';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import ErrorBoundry from '../components/ErrorBoundry';
 import Scroll from '../components/Scroll'
 
-
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchField:''
-		}
-	//	console.log('constructor');
+import { setSearchField, requestRobots } from '../action'
+// use a predefined keyword like 'mapStateToProps' and assign searchField and its state.
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
-	
-	
+}
+// MapDispatchToProps uses dispatch to provide SearchChange Event and this will call the value need.
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		// Request Robots reducers with action as dispatch
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+//App class extends the component which calls the super
+class App extends Component {
+	// constructor() {
+	// 	super()
+	// 	this.state = {
+	// 		robots: []
+	// 		//	searchField:''
+	// 	}
+	// 	//	console.log('constructor');
+	// }
+
+
 
 	componentDidMount() {
-	//	console.log('check');
-		fetch('https://jsonplaceholder.typicode.com/albums')
-		.then(response => response.json())
-		.then(albums => this.setState({ robots: albums}));
-	
-	//console.log('componentDidMount');
+		//console.log(this.props.store.getState())
+		//	console.log('check');
+		this.props.onRequestRobots();
+		//Async request from API and setting to the state called robots
+		// fetch('https://jsonplaceholder.typicode.com/albums')
+		// 	.then(response => response.json())
+		// 	.then(albums => this.setState({ robots: albums }));
+
+		//console.log('componentDidMount');
 	}
-	
-	onSearchChange = (event) => {
-		this.setState({ searchField: event.target.value });
-		
-	}
+
 	render() {
-		const { robots, searchField } = this.state;
+		//const { robots } = this.state;
+		// eslint-disable-next-line
+		const { searchField, onSearchChange, robots, isPending } = this.props;
 		const filterRobots = robots.filter(robots => {
 			return robots.title.toLowerCase().includes(searchField.toLowerCase());
 		})
@@ -42,15 +63,14 @@ class App extends Component {
 			(
 				<div className='tc'>
 					<h1 className="f2">Robo Friends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+					<SearchBox searchChange={onSearchChange} />
 					<Scroll>
-					<ErrorBoundry>
-						<CardList robots={filterRobots} />
+						<ErrorBoundry>
+							<CardList robots={filterRobots} />
 						</ErrorBoundry>
 					</Scroll>
 				</div>
 			);
-		}
-
+	}
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { Provider } from 'react-redux';
 //import Card from './Card';
 //import CardList from './CardList';
+// import creatstore & middleware application from redux
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+//create a logger using redux-logger
+import { createLogger } from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
+
+// Reducer requests robots
+// Importing tachyons CSS kit for working
 import 'tachyons';
 //import {robots} from './robots';
+
 import App from './containers/App';
 
+//getting searchrobots module from reducers file
+import { searchRobots, requestRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Thunk middleware from redux thunk file
+import thunkMiddleware from 'redux-thunk';
+// middleware logger
+const logger = createLogger();
+
+// combined reducers const file
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+//use createStore as object name with searchRobots & middleware as the parameters
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+		, document.getElementById('root'));
 registerServiceWorker();
+
